39dbdfbaf6d62a12f6c9dfdf701382a8772c167b
parishkarnagrikar-inspira <parishkar.nagrikar@inspiraenterprise.com>
2020-09-16 11:18:40 +0530

    letest pull

---
+++ src/main/java/com/mojo/mysqltest/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.inspira.mysqltest;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(" HelloWorld!!! ");
+    }
+}
---
+++ src/main/java/com/mojo/mysqltest/MysqlConnect.java
@@ -0,0 +1,23 @@
+package com.inspira.mysqltest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MysqlConnect {
+    public static void main(String[] args) throws ClassNotFoundException {
+        //Class.forName("com.mysql.jdbc.Driver");
+        String user = "root";
+        String url = "jdbc:mysql://localhost:3306/soc0";
+        String password = "root";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            if (conn != null) {
+                System.out.println("Connected to the database");
+            }
+        } catch (SQLException ex) {
+            System.out.println("An error occurred. Maybe user/password is invalid");
+            ex.printStackTrace();
+        }
+    }
+}
